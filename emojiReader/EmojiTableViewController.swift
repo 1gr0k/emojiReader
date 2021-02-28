675cebb76265b72c557dd388f640f398abb29265
Andrey Kalyamin <kalyamin.andrey@gmail.com>
2021-02-28 17:04:36 +0400

    add New cell function

--- emojiReader/EmojiTableViewController.swift
+++ emojiReader/EmojiTableViewController.swift
@@ -27,6 +27,18 @@ class EmojiTableViewController: UITableViewController {
         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
     
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        guard unwindSegue.identifier == "saveSegue" else { return }
+        let sourceVC = unwindSegue.source as! NewEmojiTableViewController
+        let emoji = sourceVC.emoji
+        
+        let newIndexPath = IndexPath(row: objects.count, section: 0)
+        objects.append(emoji)
+        tableView.insertRows(at: [newIndexPath], with: .fade)
+        
+        
+    }
+    
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
--- emojiReader/NewEmojiTableViewController.swift
+++ emojiReader/NewEmojiTableViewController.swift
@@ -9,24 +9,40 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
     
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
+    
     @IBOutlet weak var emojiTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    @IBAction func textChanged(_ sender: UITextField) {
+        
+        updateSaveButtonState()
     }
     
-
+    private func updateSaveButtonState(){
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        let emoji = emojiTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourite: self.emoji.isFavourite)
+    }
+    
 }
